aca0c0fb342aa475b9735edcd02716b2267b4635
Matthew Jones <Matthew_Jones@trimble.com>
2017-04-27 13:41:54 -0600

    Pushing new changes

--- source/utility/matching/ComparableMatchFactory.cs
+++ source/utility/matching/ComparableMatchFactory.cs
@@ -5,15 +5,32 @@ namespace code.utility.matching
   public class ComparableMatchFactory<Item, Property> where Property : IComparable<Property>
   {
     IGetTheValueOfAProperty<Item, Property> accessor;
+    MatchFactory<Item, Property> matchFactory;
 
     public ComparableMatchFactory(IGetTheValueOfAProperty<Item, Property> accessor)
     {
+      this.matchFactory = new MatchFactory<Item, Property>(accessor);
       this.accessor = accessor;
     }
 
     public Criteria<Item> greater_than(Property value)
     {
       return x => accessor(x).CompareTo(value) > 0;
+    }
+
+    public Criteria<Item> equal_to(Property value)
+    {
+      return matchFactory.equal_to(value);
+    }
+
+    public Criteria<Item> equal_to_any(params Property[] values)
+    {
+      return matchFactory.equal_to_any(values);
+    }
+
+    public Criteria<Item> not_equal_to(Property value)
+    {
+      return matchFactory.not_equal_to(value);
     }
   }
 }
